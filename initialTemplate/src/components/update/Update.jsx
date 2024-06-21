101d47ec462e65326e29a4cb019ddf205b76a27a
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-06-21 20:12:43 +0530

    local redux

--- initialTemplate/src/components/update/Update.jsx
+++ initialTemplate/src/components/update/Update.jsx
@@ -2,17 +2,38 @@ import React from "react";
 import Warning from "../warning/Warning";
 import "./update.css";
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { update, remove } from "../../redux/userSlice";
 
 export default function Update() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const user = useSelector((state) => state.user);
 
+  const dispatch = useDispatch();
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    console.log(name);
+
+    dispatch(update({ name, email }));
+  };
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    dispatch(remove());
+    console.log("hi");
+  };
+  // console.log(name);
+  // console.log(email);
   return (
     <div className="update">
       <div className="updateWrapper">
         <h3 className="updateTitle">Update Your Account</h3>
         <Warning />
-        <button className="delete">Delete Account</button>
+        <button className="delete" onClick={handleDelete}>
+          Delete Account
+        </button>
         <div className="updateContainer">
           <form>
             <div className="formItem">
@@ -31,7 +52,8 @@ export default function Update() {
               <input
                 className="formInput"
                 type="text"
-                placeholder="John"
+                placeholder={user.name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div className="formItem">
@@ -39,16 +61,15 @@ export default function Update() {
               <input
                 className="formInput"
                 type="text"
-                placeholder="john@gmail.com"
+                placeholder={user.email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="formItem">
               <label>Password</label>
               <input className="formInput" type="password" />
             </div>
-            <button
-              className="updateButton"
-            >
+            <button className="updateButton" onClick={handleUpdate}>
               Update
             </button>
           </form>
